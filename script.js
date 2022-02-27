9150bd7b3f73eabb078b7f90428accc219de0bf2
rohit sherkar <rohitsherkar3@gmail>
2022-02-27 14:22:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// let outputScreen=document.getElementById("output-screen");
+
+
+
+// function display(num){
+//     outputScreen.value+=num;
+// }
+// function calculate(){
+//     try{
+//         outputScreen.value=eval(outputScreen.value);
+//     }
+//     catch(err){
+//         alert("invalid")
+//     }
+// }
